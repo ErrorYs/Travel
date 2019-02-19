31a8a267ea02be6704eafb76ad07765328c5041d
ys <1101998350@qq.com>
2019-02-19 22:50:59 +0800

    city头部和搜索部分完成

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Header></Header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import CitySearch from './components/Search'
 export default {
   components: {
-    Header
+    Header,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+@import "~styles/varibles.less";
+.search {
+  height: .32rem;
+  padding: .05rem;
+  background-color: @bgColor;
+  .search-input {
+    width: 100%;
+    box-sizing: border-box;
+    height: 0.3rem;
+    border-radius: .03rem;
+    // text-indent: .1rem;
+    padding: .011rem 0;
+    font-size: .16px;
+    line-height: .3rem;
+    text-align: center
+  }
+}
+</style>
